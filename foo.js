a53f2b343f67cfecd32da5cc4b9f2a6445c35771
丁科 <dk@192.168.2.104>
2014-06-30 00:57:15 +0800

    update js

--- foo.js
+++ foo.js
@@ -1,7 +1,5 @@
-var body=document.getElementsByTagName("body")[0];
-alert(document.getElementById("body")[0].innerHTML);
-var content=body.innerHTML;
 
-var storage = window.localStorage;
-var myDate = new Date();
-storage.setItem(myDate.getTime(),content);
+console.info("start");
+var content= document.body.innerHTML;
+console.info(content);
+alert(content);
